e5010da55f617ca870ddfeefbb0fefad25252ad9
lizandro.arcos5100 <lizandro.arcos5100@utc.edu.ec>
2024-01-04 02:52:47 -0500

    rede

--- navidad/settings.py
+++ navidad/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 if 'RENDER' in os.environ:
     DEBUG = False
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -131,9 +132,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'navidad', 'static'),
 ]
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
